fe314addf1162f11e1c592399ec03e214c6abf14
Yunwei Dai <daiyunwei1998@gmail.com>
2023-11-17 03:44:57 +0800

    Implement maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -155,6 +155,15 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for(int i = 0;i < b.size(); i+=1){
+            for(int j = 0;j < b.size(); j+=1){
+                if(b.tile(i,j) != null){
+                    if(b.tile(i,j).value() == MAX_PIECE){
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
