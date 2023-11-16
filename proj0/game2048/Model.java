6935ed03073d2c69126a0eacf6afda0ed71106cf
Yunwei Dai <daiyunwei1998@gmail.com>
2023-11-17 03:28:57 +0800

    Implement Model.testEmptySpace

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for(int i = 0;i < b.size(); i+=1){
+            for(int j = 0;j < b.size(); j+=1){
+                if(b.tile(i,j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
