200a37f71c3940bb0ba28dbdeafd1299e39669d3
Yunwei Dai <daiyunwei1998@gmail.com>
2023-12-06 02:45:23 +0800

    MaxArrayDeque

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,36 @@
+package deque;
+import java.util.Comparator;
+
+public class MaxArrayDeque<T extends Comparable<T>> extends ArrayDeque<T>{
+
+    private Comparator<T> c;
+    public MaxArrayDeque(Comparator<T> c){
+        this.c = c;
+    }
+    public T max(){
+        if(this.isEmpty()){
+            return null;
+        }else{
+            T max = this.get(0);
+            for (T element : this) {
+                if (c.compare(element, max) > 0) {
+                    max = element;
+                }
+            }
+            return max;
+        }
+    }
+    public T max(Comparator<T> c){
+        if(this.isEmpty()){
+            return null;
+        }else{
+            T max = this.get(0);
+            for (T element : this) {
+                if (c.compare(element, max) > 0) {
+                    max = element;
+                }
+            }
+            return max;
+        }
+    }
+}
